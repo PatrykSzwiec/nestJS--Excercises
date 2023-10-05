8ed24ee4cd09b67589855e767c8109ada03d9493
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-10-05 20:44:07 +0200

    Create endpoints for products

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { ProductsController } from './products/products.controller';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [AppController, ProductsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
