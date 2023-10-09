5a9475ce5a1323b6ed2d507c9a7181fc08113a10
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-10-09 20:03:41 +0200

    Change Product endpoints, add GET extended Products endpoint

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/shared/services/prisma.service';
 import { Order } from '@prisma/client';
+import { BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class OrdersService {
@@ -23,18 +24,24 @@ export class OrdersService {
     });
   }
 
-  public create(
+  public async create(
     orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<Order> {
     const { productId, ...otherData } = orderData;
-    return this.prismaService.order.create({
-      data: {
-        ...otherData,
-        product: {
-          connect: { id: productId },
+    try {
+      return await this.prismaService.order.create({
+        data: {
+          ...otherData,
+          product: {
+            connect: { id: productId },
+          },
         },
-      },
-    });
+      });
+    } catch (error) {
+      if (error.code === 'P2025')
+        throw new BadRequestException("Product doesn't exist");
+      throw error;
+    }
   }
 
   public updateById(
